bad1e1a8afbcd840f6dc43e5775039c19733973d
baonn <none@none>
2010-05-31 16:01:56 +0000

    get rid of log4j.properties
    --HG--
    extra : convert_revision : svn%3Acc9e4985-2604-4ca3-9a6b-1c23c61bd3f4/GUITARModel-Core/trunk%402478

--- src/edu/umd/cs/guitar/util/GUITARLog.java
+++ src/edu/umd/cs/guitar/util/GUITARLog.java
@@ -3,17 +3,64 @@
  */
 package edu.umd.cs.guitar.util;
 
-import org.apache.log4j.Logger;
+import java.io.IOException;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 
 /**
  * Global runtime log for GUITAR
  * 
  * <p>
- *  
+ * 
  * @author Bao Nguyen
- *
+ * 
  */
 public class GUITARLog {
-    public static Logger log =Logger.getLogger(GUITARLog.class);
+	/**
+	 * 
+	 */
+	public static final String LOGFILE_NAME_SYSTEM_PROPERTY = "logfile.name";
+	/**
+	 * 
+	 */
+	private static final String LOG_LAYOUT_PATTER = "%-6r [%t] %-5p - %m%n";
+	/**
+	 * 
+	 */
+	private static final String GUITAR_DEFAULT_LOG = "GUITAR-Default.log";
+	public static Logger log;
+	private static Level level = Level.DEBUG;
+
+	static {
+		log = Logger.getLogger(GUITARLog.class);
+
+		PatternLayout layout = new org.apache.log4j.PatternLayout();
+		layout.setConversionPattern(LOG_LAYOUT_PATTER);
+
+		ConsoleAppender stdout = new ConsoleAppender(layout);
+		log.addAppender(stdout);
+
+		FileAppender file = null;
+
+		String logFileName = System.getProperty(LOGFILE_NAME_SYSTEM_PROPERTY);
+		
+		if (logFileName == null) {
+			logFileName = GUITAR_DEFAULT_LOG;
+		}
+
+		try {
+			file = new FileAppender(layout, logFileName, false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		log.addAppender(file);
+
+		log.setLevel(level);
+	}
+
 }
