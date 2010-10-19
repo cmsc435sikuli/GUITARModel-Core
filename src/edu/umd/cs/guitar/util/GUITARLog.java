4f8c38ee9347c1cb29de7bdcb99042f39b4a49d0
ishanbanerjee <none@none>
2010-10-19 23:03:29 +0000

    GUITARLog edit
    --HG--
    extra : convert_revision : svn%3Acc9e4985-2604-4ca3-9a6b-1c23c61bd3f4/GUITARModel-Core/trunk%402517

--- src/edu/umd/cs/guitar/util/GUITARLog.java
+++ src/edu/umd/cs/guitar/util/GUITARLog.java
@@ -1,3 +1,28 @@
+/*
+ *  Copyright (c) 2009-@year@. The  GUITAR group  at the University of
+ *  Maryland. Names of owners of this group may be obtained by sending
+ *  an e-mail to atif@cs.umd.edu
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files 
+ *  (the "Software"), to deal in the Software without restriction,
+ *  including without limitation  the rights to use, copy, modify, merge,
+ *  publish,  distribute, sublicense, and/or sell copies of the Software,
+ *  and to  permit persons  to whom  the Software  is furnished to do so,
+ *  subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO  EVENT SHALL THE  AUTHORS OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR  OTHER LIABILITY,  WHETHER IN AN  ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 /**
  * 
  */
@@ -20,57 +45,87 @@ import org.apache.log4j.PatternLayout;
  * 
  */
 public class GUITARLog {
-	
-	/**
-	 * 
-	 */
-	public static final String LOGFILE_NAME_SYSTEM_PROPERTY = "logfile.name";
-	
-	public static Logger log ;//= Logger.getLogger("GUITAR");
-	
-	
-	/**
-	 * 
-	 */
-	private static final String LOG_LAYOUT_PATTERN = "%-6r [%t] %-5p - %m%n";
-	/**
-	 * 
-	 */
-	private static final String GUITAR_DEFAULT_LOG = "GUITAR-Default.log";
+ 
+   /**
+    * 
+    */
+   public static final
+   String LOGFILE_NAME_SYSTEM_PROPERTY = "logfile.name";
+ 
+   public static
+   Logger log;
+ 
+ 
+   /**
+    * 
+    */
+   private static final
+   String LOG_LAYOUT_PATTERN = "%-6r [%t] %-5p - %m%n";
 
-	/**
-	 * Logging level
-	 */
-	private static Level level = Level.DEBUG;
-//	private static Level level = Level.INFO;
 
-	static {
-		
-		log = Logger.getLogger("GUITARLog");
+   /**
+    * 
+    */
+   private static final
+   String GUITAR_DEFAULT_LOG = "GUITAR-Default.log";
 
-		PatternLayout layout = new org.apache.log4j.PatternLayout();
-		layout.setConversionPattern(LOG_LAYOUT_PATTERN);
 
-		ConsoleAppender stdout = new ConsoleAppender(layout);
-		log.addAppender(stdout);
+   /**
+    * Logging level
+    */
+   private static
+   Level level = Level.DEBUG;
 
-		FileAppender file = null;
 
-		String logFileName = System.getProperty(LOGFILE_NAME_SYSTEM_PROPERTY);
+   static {
+      log = Logger.getLogger("GUITARLog");
 
-		if (logFileName == null) {
-			logFileName = GUITAR_DEFAULT_LOG;
-		}
+      PatternLayout layout = new org.apache.log4j.PatternLayout();
+      layout.setConversionPattern(LOG_LAYOUT_PATTERN);
 
-		try {
-			file = new FileAppender(layout, logFileName, false);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		log.addAppender(file);
+      ConsoleAppender stdout = new ConsoleAppender(layout);
+      log.addAppender(stdout);
 
-		log.setLevel(level);
-		
-	}
+      FileAppender file = null;
 
+      String logFileName = System.getProperty(LOGFILE_NAME_SYSTEM_PROPERTY);
+
+      if (logFileName == null) {
+         logFileName = GUITAR_DEFAULT_LOG;
+      }
+
+      try {
+         file = new FileAppender(layout, logFileName, false);
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
+      log.addAppender(file);
+      log.setLevel(level);
+   }
+
+   /**
+    * Log the given string 'str' as Info.
+    *
+    * @return
+    */
+   public static
+   void Info(Level level, String str) {
+      if (level == GUITARLog.level) {
+         System.out.println("Info: " + str);
+      }
+   }
+
+   /**
+    * Log the given string 'str' as Error.
+    *
+    * @return
+    */
+   public static
+   void Error(String str) {
+      System.out.println("Error: " + str);
+   }
+
+
+   // End of class
 }
